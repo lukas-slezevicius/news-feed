8d5e58fa31e9345b139039c4495e2e705d32be2b
Lukas Slezevicius <lukas@slezevicius.com>
2022-07-17 19:35:45 +0100

    Can fetch and parse web feeds

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let url = args.get(1).expect("Invalid url");
+    let body = reqwest::blocking::get(url)
+        .unwrap()
+        .bytes()
+        .unwrap()
+        .to_vec();
+    let model = feed_rs::parser::parse(&mut &body[..]).expect("Could not parse feed file");
+    for entry in model.entries.iter() {
+        if let Some(title) = &entry.title {
+            print!("{} ", title.content);
+        }
+        for link in &entry.links {
+            print!("{} ", link.href);
+        }
+        println!();
+    }
 }
